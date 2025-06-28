c6202c1dcbec3e073f4d6b38f4de73db9631b7d1
Antonio Mir_S <antoniomir534@gmail.com>
2025-06-28 02:41:22 -0400

    Eliminacion de home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 import Login from './pages/Login/login';
 import Principal from './pages/Principal/principal';
 
@@ -41,12 +40,6 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
       </IonRouterOutlet>
         <Route exact path="/login">
           <Login />
